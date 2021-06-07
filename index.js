39d8c55e57f1fb7f53c0de1727ed724c08ac8bb2
Mac Rowe <m.rowe42@gmail.com>
2021-06-07 09:53:28 -0400

    commit tree 5740b4315e3b7c507ee4e04449fc5f6628e2b2c7

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string) {
+    string = string[0].toUpperCase() + string.slice(1)
+    let wordsArray = string.split(" ")
+    let lowerCaseWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+ 
+
+    for(let i = 0; i < wordsArray.length; i++) {
+      if(!(lowerCaseWords.includes(wordsArray[i]))) {
+        wordsArray[i] = wordsArray[i][0].toUpperCase() + wordsArray[i].slice(1);
+      }
+    }
+    return wordsArray.join(" ")
+  }
+    
+
+
 }
